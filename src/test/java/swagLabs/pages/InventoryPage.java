3e0efb5d4767f8fed0a6f3f4e2466b57a74beb2a
molottva <molottva@list.ru>
2022-08-13 16:24:35 +0300

    Added first test

--- src/test/java/swagLabs/pages/InventoryPage.java
+++ src/test/java/swagLabs/pages/InventoryPage.java
@@ -1,4 +1,36 @@
 package swagLabs.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class InventoryPage {
+    private final WebDriver driver;
+    private final HeaderComponent header;
+    private final HeaderProductsComponent headerProducts;
+    private final WebElement inventoryList;
+    private final List<WebElement> inventoryListItems;
+
+    public InventoryPage(WebDriver driver) {
+        this.driver = driver;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //todo переделать проверку с url на более правильную
+        assertTrue(wait.until(ExpectedConditions.urlContains("https://www.saucedemo.com/inventory")));
+
+        this.header = new HeaderComponent(driver);
+        this.headerProducts = new HeaderProductsComponent(driver);
+        this.inventoryList = driver.findElement(By.cssSelector("div.inventory_list"));
+        this.inventoryListItems = inventoryList.findElements(By.cssSelector("div.inventory_item"));
+
+        assertTrue(inventoryList.isEnabled());
+        assertTrue(inventoryList.isEnabled());
+    }
 }
--- src/test/java/swagLabs/pages/MainPage.java
+++ src/test/java/swagLabs/pages/MainPage.java
@@ -6,37 +6,52 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import swagLabs.helpers.DataHelper;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainPage {
-    private static final WebDriver driver = new ChromeDriver();
-    private static final WebElement loginForm = driver.findElement(By.cssSelector("div[id='login_button_container']"));
-    private static final WebElement usernameInput = loginForm.findElement(By.cssSelector("input[id='user-name']"));
-    private static final WebElement passwordInput = loginForm.findElement(By.cssSelector("input[id='password']"));
-    private static final WebElement loginButton = loginForm.findElement(By.cssSelector("input[data-test='login-button']"));
-    private static final WebElement errorNotification = loginForm.findElement(By.cssSelector("h3[data-test='error']"));
-    private static final WebElement errorButton = errorNotification.findElement(By.cssSelector("button.error-button"));
+    private final WebDriver driver;
+    private final WebElement loginForm;
+    private final WebElement usernameInput;
+    private final WebElement passwordInput;
+    private final WebElement loginButton;
+    private final WebElement errorMessage;
+//    private final WebElement errorNotification;
+//    private final WebElement errorButton;
 
-    //todo посмотреть ExpectedCondition
-    public MainPage() {
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //todo переделать проверку с url на более правильную
+        assertTrue(wait.until(ExpectedConditions.urlToBe("https://www.saucedemo.com/")));
+
+        this.loginForm = driver.findElement(By.cssSelector("div[id='login_button_container']"));
+        this.usernameInput = loginForm.findElement(By.cssSelector("input[id='user-name']"));
+        this.passwordInput = loginForm.findElement(By.cssSelector("input[id='password']"));
+        this.loginButton = loginForm.findElement(By.cssSelector("input[data-test='login-button']"));
+        this.errorMessage = loginForm.findElement(By.cssSelector("div.error-message-container"));
+
+        //todo перенести в проверку с невалидными данными пользователя
+//        this.errorNotification = loginForm.findElement(By.cssSelector("h3[data-test='error']"));
+//        this.errorButton = errorNotification.findElement(By.cssSelector("button.error-button"));
+
         assertTrue(loginForm.isDisplayed());
         assertTrue(usernameInput.isDisplayed());
         assertTrue(passwordInput.isDisplayed());
         assertTrue(loginButton.isDisplayed());
-        assertFalse(errorNotification.isEnabled());
-        assertFalse(errorButton.isEnabled());
+
+        assertEquals("error-message-container", errorMessage.getAttribute("class"));
     }
 
-    public InventoryPage assertSuccessLogin(DataHelper.UserLoginData user) {
-        usernameInput.sendKeys(user.getLogin());
-        passwordInput.sendKeys(user.getPassword());
-        loginButton.click();
+    public InventoryPage assertSuccessLogin(String login, String password) {
+        usernameInput.sendKeys(login);
+        passwordInput.sendKeys(password);
 
-        assertFalse(errorNotification.isEnabled());
-        assertFalse(errorButton.isEnabled());
-        return new InventoryPage();
+        loginButton.click();
+        return new InventoryPage(driver);
     }
 }
